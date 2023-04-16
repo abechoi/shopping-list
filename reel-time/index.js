eec174712d910fa007fcd79210c13d426663c6d6
Abe Choi <abraham.choi@icloud.com>
2023-04-15 22:38:26 -0700

    added getEnv

--- reel-time/index.js
+++ reel-time/index.js
@@ -1,22 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-import { getDbUrl } from "./env.js";
-
+import { getEnv } from "./env.js"
+const env = getEnv()
 const appSettings = {
-    databaseURL: getDbUrl()
-  }
+    databaseURL: env.DB_URL
+}
 
-  const app = initializeApp(appSettings);
-  const database = getDatabase(app);
-  const moviesInDB = ref(database, 'movies');
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, 'movies')
 
-  // create an event listener to console log the text value from id="input-fields" when id="add-button" is clicked
-  const inputField = document.getElementById("input-field")
-  const addButton = document.getElementById("add-button")
+// create an event listener to console log the text value from id="input-fields" when id="add-button" is clicked
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
 
-  addButton.addEventListener("click", function() {
-    let inputValue = inputField.value;
+addButton.addEventListener("click", function() {
+    let inputValue = inputField.value
     push(moviesInDB, inputValue)
-    console.log(`${inputValue} added to database!`);
-  })
+    console.log(`${inputValue} added to database!`)
+})
